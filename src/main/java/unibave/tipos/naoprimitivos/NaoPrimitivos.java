f0814f38223756024b6ce6ef88afc491c3e2bd71
joaao.castelli@gmail.com <castelli13>
2020-03-16 13:46:57 -0300

    -> Class type of variables

---
+++ src/main/java/unibave/tipos/naoprimitivos/NaoPrimitivos.java
@@ -0,0 +1,18 @@
+package main.java.unibave.tipos.naoprimitivos;
+
+public class NaoPrimitivos {
+
+    public static void main(String[] args) {
+
+        String texto = "Meu texto para apresentação";
+
+        Void v;
+
+        Object o = new Object();
+
+        Number numero = Integer.valueOf(100);
+
+        numero.toString();
+    }
+
+}
